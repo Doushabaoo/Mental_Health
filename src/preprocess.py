7b1ceb7842b2ae2675b9500a953b17dd28fde985
Charles Yang <charles.yang@ensiie.eu>
2025-06-04 12:17:29 +0200

    first submission to test

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,7 +1,9 @@
 import numpy as np 
 import pandas as pd 
+from sklearn.base import BaseEstimator, TransformerMixin
 
 def assign_pressure(data):
+    data=data.copy()
     # Step 1: Create masks for students and professionals
     is_student = data["Working Professional or Student"] == "Student"
     is_professional = data["Working Professional or Student"] == "Working Professional"
@@ -24,6 +26,7 @@ def assign_pressure(data):
     return data 
 
 def assign_satisfaction(data):
+    data=data.copy()
     # Step 1: Create masks for students and professionals
     is_student = data["Working Professional or Student"] == "Student"
     is_professional = data["Working Professional or Student"] == "Working Professional"
@@ -45,9 +48,8 @@ def assign_satisfaction(data):
 
     return data 
 
-
-
-def fill_profession(data): 
+def fill_profession(data):
+    data=data.copy() 
     data["Profession"] = np.where(
         data["Working Professional or Student"] == "Working Professional",
         data["Profession"],
@@ -57,6 +59,7 @@ def fill_profession(data):
     return data 
 
 def replace_diet_habits(data):
+    data=data.copy()
     top_categories = ["Moderate", "Unhealthy", "Healthy"]
     data['Dietary Habits'] = np.where(
         data['Dietary Habits'].isin(top_categories),
@@ -67,6 +70,7 @@ def replace_diet_habits(data):
     return data 
 
 def replace_sleep_duration(data):
+    data=data.copy()
     top_categories = ["Less than 5 hours", "7-8 hours", "More than 8 hours", "5-6 hours"]
     data["Sleep Duration"] = np.where(
         data["Sleep Duration"].isin(top_categories),
@@ -75,3 +79,22 @@ def replace_sleep_duration(data):
     )
 
     return data 
+
+class GroupImputer(BaseEstimator, TransformerMixin):
+    def __init__(self, cols_to_impute):
+        self.cols_to_impute = cols_to_impute
+        self.medians = {} # storage for learned values
+    
+    def fit(self, X, y=None):
+        for col in self.cols_to_impute:
+            self.medians[col] = X.groupby("Working Professional or Student")[col].median().to_dict()
+        return self
+    
+    def transform(self, X):
+        X = X.copy()
+        for col in self.cols_to_impute: 
+            missing_mask = X[col].isna()
+            for group, median_val in self.medians[col].items():
+                group_mask = (X["Working Professional or Student"] == group) & missing_mask
+                X.loc[group_mask, col] = median_val
+        return X
